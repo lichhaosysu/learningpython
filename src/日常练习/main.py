529fbc83a368adb34bc4675c94f46da322de9836
lichhaosysu <lichhaosysu@qq.com>
2013-05-17 15:16:14 +0800

    change the main.py

--- src/日常练习/main.py
+++ src/日常练习/main.py
@@ -1,30 +1,11 @@
-from xml.sax.handler import ContentHandler
-from xml.sax import parse
+def be_excellent(wrapped):
+    def wrapper(*args, **kwargs):
+        print "Be excellent to each other..."
+        return wrapped(*args, **kwargs)
+    return wrapper
 
-class PageMaker(ContentHandler):
-    passthrough = False
-    def startElement(self, name, attrs):
-        if name == 'page':
-            self.passthrough = True
-            self.out = open(attrs['name'] + '.html', 'w')
-            self.out.write('<html><head>\n')
-            self.out.write('<title>%s</title>\n' % attrs['title'])
-            self.out.write('</head><body>\n')
-        elif self.passthrough:
-            self.out.write('<' + name)
-            for key, val in attrs.items():
-                self.out.write(' %s="%s"' % (key, val))
-            self.out.write('>')
-            
-    def endElement(self, name):
-        if name == 'page':
-            self.passthrough = False
-            self.out.write('\n</body></html>\n')
-            self.out.close()
-        elif self.passthrough:
-            self.out.write('</%s>' % name)
-            
-    def characters(self, chars):
-        if self.passthrough: self.out.write(chars)
+@be_excellent
+def party_on(who):
+    print "...and party on, {0}!".format(who)
 
-parse('website.xml', PageMaker())
+party_on('guys')
